0ede61acb366f3a26adadb1c559dd93e2f3fd292
timmaffett <timmaffett@gmail.com>
2023-04-19 22:12:23 -0700

    move to 1.0.4 custommousecursor

--- custom_mouse_cursor/flutter_service_worker.js
+++ custom_mouse_cursor/flutter_service_worker.js
@@ -43,9 +43,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "91a75f4b4662062f21e147625b0ddd16
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "456c446127cc43df744caa3d2046b55b",
-"/": "456c446127cc43df744caa3d2046b55b",
-"main.dart.js": "e829678cab0c8352750b47dcfb965ded",
+"index.html": "5e9d18954bc17be9e36d60f93347a0d6",
+"/": "5e9d18954bc17be9e36d60f93347a0d6",
+"main.dart.js": "4c431274e1e78b0fc0b48225cb5e9bee",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "87566a2baf6bccb66deaa054654e642d"};
 // The application shell files that are downloaded before a service worker can
