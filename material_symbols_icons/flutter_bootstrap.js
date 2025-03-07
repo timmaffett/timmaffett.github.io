8ccb65e0168f301987219429c57e46c890803b39
timmaffett <timmaffett@gmail.com>
2025-03-06 20:24:11 -0800

    update to official font version 2.808

--- material_symbols_icons/flutter_bootstrap.js
+++ material_symbols_icons/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"92de3d0f8830fa92053bfbe4b8de52ad962ff52e","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"87196771eb0119ffc9d23a644bd4209eb19d5eef","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3065163604"
+    serviceWorkerVersion: "1464471796"
   }
 });
